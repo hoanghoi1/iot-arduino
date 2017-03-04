2646b10c4321235594256cd2a40c32f1da0410da
Hoang Hoi <hoanghoi1310@gmail.com>
2017-03-04 17:06:27 +0700

    init

---
+++ CoAPExample/CoAPExample.ino
@@ -0,0 +1,194 @@
+#include <IPv6Stack.h>
+#include <XBeeMACLayer.h>
+
+#define IS_INTERMEDIATE_ROUTER  (UIP_CONF_ROUTER && 0)// FOR INTERMEDIATE ROUTERS: 1, FOR NODES: 0 -> UIP_CONF_ROUTER MUST BE 1
+
+#define IS_BORDER_ROUTER (UIP_CONF_ROUTER && !IS_INTERMEDIATE_ROUTER)
+
+#define UDP_PORT 5683
+
+#define SEND_INTERVAL		(10 * 1000)// 10 seconds
+
+//This function calculates the RAM memory left
+int mem(){
+  uint8_t * heapptr, * stackptr;
+  stackptr = (uint8_t *)malloc(4);
+  heapptr = stackptr;
+  free(stackptr);
+  stackptr = (uint8_t *)(SP);
+  return stackptr - heapptr;
+}
+
+//We need a specific object to implement the MACLayer interface (the virtual methods). In this case we use XBee but could be anyone capable of implementing that interface
+XBeeMACLayer macLayer;
+
+//We use a buffer in order to put the data that we will send
+#define UDP_MAX_DATA_LEN 100
+char udp_send[UDP_MAX_DATA_LEN];
+
+//This will be the destination IP address
+IPv6Address addr_dest(0xaa, 0xaa, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+
+#if IS_BORDER_ROUTER
+  //If we are configured to be a router, this is our prefix. Up to now we hardcode it here. Note: The aaaa::/64 prefix is the only known by the 6loPAN context so it will achieve a better compression.
+  IPv6Address prefix(0xaa, 0xaa, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+#endif
+
+String coap_uri = "uri_test";       // CoAP Option: URI
+String coap_query = "apikey=fd205ed589";// CoAP Option: Query. Emoncms: "apikey=<your_api_key>"
+String coap_query_opt = "node=1";   // CoAP Option: Query (Optional)
+String coap_payload_string = "";   // CoAP Payload.
+
+//We use a timer in order to send data if we do not have to respond to a message received.
+IPv6Timer send_timer;
+
+char udp_data[UDP_MAX_DATA_LEN];
+int udp_data_length = 0;
+IPv6Address sender_address;
+uint16_t dest_port = UDP_PORT;
+
+//changes the /64 prefix of the given address to the value of the prefix that was given to us by a router (if any). Returns true if we have an address with a prefix like this and false if we did not get a prefix and thus a global address
+bool change_addr_prefix(IPv6Address &address){
+  IPv6Address globalAddress;
+  if (IPv6Stack::getGlobalPreferredAddress(globalAddress)){//we get our global address' prefix, given by our router, to send the response to the sender using global addresses (this will allow us to see routing)
+    address.setAddressValue(globalAddress.getAddressValue(0), 0);
+    address.setAddressValue(globalAddress.getAddressValue(1), 1);
+    address.setAddressValue(globalAddress.getAddressValue(2), 2);
+    address.setAddressValue(globalAddress.getAddressValue(3), 3);
+    return true;
+  }
+  return false;
+}
+
+//This is what we will run as a result of receiving a UDP message. We first use the serial port to show what we received, wait half of the sending time and send the response to the sender that is, actually, the same message inverted
+void udp_callback(char *data, int datalen, int sender_port, IPv6Address &sender_addr){
+  Serial.println(mem());
+  delay(100);
+  //Show received dada
+  data[datalen] = 0;
+  Serial.println();
+  Serial.println();
+  Serial.print("Data received from ip: ");
+  sender_addr.print();
+  Serial.print(" port: ");
+  Serial.print(sender_port);
+  Serial.print(", data: ");
+  //Serial.println(data); //could do it this way too, but we try the other way just to test
+  //Show data
+  while (IPv6Stack::udpDataAvailable()){
+     Serial.print(IPv6Stack::readUdpData());
+  }
+  Serial.println();
+        
+    //In case we have been given a prefix by a router and we have validated our global address, change the address of the sender by changing its prefix. It is related to the same router, messages will go through it
+  if (!change_addr_prefix(sender_addr)){
+  IPv6Address globalAddress;
+  globalAddress.print();
+    #if IS_INTERMEDIATE_ROUTER // routers do not respond until they have their prefix (Border Routers never respond)
+    return;
+    #endif
+  }
+    
+  delay(SEND_INTERVAL/2);//take SEND_INTERVAL/2 to respond
+
+  Serial.println("Sending response..");
+  Serial.println(); 
+  delay(50); 
+  int j;
+  for(j=0; j<datalen; ++j){
+    udp_send[datalen-1-j] = data[j]; 
+  }
+  addr_dest = sender_addr; //now if our message is not responded, it will be resent from the main loop to the same destination
+  dest_port = sender_port;
+  IPv6Stack::udpSend(sender_addr, sender_port, udp_send, datalen);
+  delay(50);
+  send_timer.restart();//each time we receive something we reset this timer so we should not send in the main loop as long as our message is responded
+}
+
+void setup(){  
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println();
+  delay(100);
+  
+  // init network-device
+  if (!IPv6Stack::initMacLayer(&macLayer)){
+    Serial.println("CANNOT INITIALIZE XBEE MODULE.. CANNOT CONTINUE");
+    while (true){};
+  }
+  
+  //init IP Stack
+  IPv6Stack::initIpStack();  
+  Serial.println("IPV6 INITIALIZED");
+  delay(100);
+  
+  //init UDP
+  IPv6Stack::initUdp(UDP_PORT);
+  Serial.println("UDP INITIALIZED");
+  delay(100);
+  
+  //If Border Router, set prefix. If not, set a timer to send data
+  #if !IS_BORDER_ROUTER
+      send_timer.set(SEND_INTERVAL);
+      Serial.println("SEND TIMER SET");
+      delay(50);      
+  #else
+      //This line is added to specify our prefix    
+      IPv6Stack::setPrefix(prefix, 64); 
+  #endif /*IS_BORDER_ROUTER*/
+  
+  Serial.println("SETUP FINISHED!");
+  delay(100);
+}
+
+void loop(){
+  //Always need to poll timers in order to make the IPv6 Stack work
+  IPv6Stack::pollTimers();  
+  //If we are not a router (any kind), we also send messages
+#if !UIP_CONF_ROUTER
+  if (send_timer.expired()){
+      send_timer.reset();
+      Serial.println();
+      Serial.print("MEMORY LEFT:");
+      Serial.println(mem());
+      Serial.println("Sending data..");
+      delay(50);
+      unsigned char payload[12];
+      payload[0] = 0x40;
+      payload[1] = 0x01;
+      payload[2] = 0xc3;
+      payload[3] = 0x00;
+      payload[4] = 0xb5;
+      payload[5] = 0x6c;
+      payload[6] = 0x69;
+      payload[7] = 0x6e;
+      payload[8] = 0x6b;
+      payload[9] = 0x31;
+      payload[10] = 0xc1;
+      payload[11] = 0x02;
+      // char temp[8];
+
+    // sprintf(temp, "%x", payload[0]);
+      IPv6Stack::udpSend(addr_dest, dest_port, payload, 12);
+      Serial.println("Sending CoAP data..");
+      delay(200);
+      IPv6Stack::sendCoAPpacket(addr_dest, coap_uri , coap_query , coap_query_opt , coap_payload_string);
+  }
+#endif
+  //We always check if we got anything. If we did, process that with the IPv6 Stack
+  if (IPv6Stack::receivePacket()){
+    Serial.println("receiver data");
+#if !IS_BORDER_ROUTER
+      //If we are not configured as border router, check if udp data is available and run the callback with it
+      if (IPv6Stack::udpDataAvailable()){
+        udp_data_length = IPv6Stack::getUdpDataLength();
+        IPv6Stack::getUdpData(udp_data);
+        IPv6Stack::getUdpSenderIpAddress(sender_address);
+        udp_callback(udp_data, udp_data_length, IPv6Stack::getUdpSenderPort(), sender_address);
+      }
+#endif
+  }
+  delay(100);
+}
+
+/*---------------------------------------------------------------------------*/
---
+++ IPv6Stack.cpp
@@ -0,0 +1,321 @@
+/*
+ Arduino IPv6 stack class
+ 
+ This class represents the IPv6 stack (ip, udp, rpl) which needs the implemetation of the link layer to send and receive packets
+ 
+ created 29 June 2012
+ by Alejandro Lampropulos (alejandro.lampropulos@telecom-bretagne.eu)
+ Telecom Bretagne Rennes, France
+ 
+ Copyright (c) 2012, Telecom Bretagne Rennes, France.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 1. Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 3. Neither the name of the Institute nor the names of its contributors
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+ 
+ */
+
+
+extern "C" {
+#include "uip.h"
+#include "sicsLowPan.h"
+#include "uip_icmp6.h"
+#include "uip_ds6.h"
+#include "uip_nd6.h"
+#include "uip_arp.h"
+#include "ctimer.h"
+#include "neighbor_info.h"
+#include "rpl.h"
+#include <string.h> 
+}
+#include "XBee.h"
+#include "IPv6Stack.h"
+#include "arduino_debug.h"
+
+#ifndef NULL
+#define NULL 0
+#endif /* NULL */
+
+MACLayer* IPv6Stack::mac;
+struct simple_udp_connection IPv6Stack::broadcast_connection;
+
+IPv6llAddress IPv6Stack::ll_destination_address;
+IPv6llAddress IPv6Stack::ll_source_address;
+
+const uint8_t* IPv6Stack::udp_data;
+uint16_t IPv6Stack::udp_data_length = 0;
+uint16_t IPv6Stack::udp_data_length_left = 0;
+const uip_ipaddr_t* IPv6Stack::sender_addr;
+uint16_t IPv6Stack::sender_port;
+char IPv6Stack::packetBuffer[ COAP_PACKET_SIZE ]; // Buffer to hold outgoing packets
+unsigned int IPv6Stack::messageID; // messageID for coap packets
+
+bool IPv6Stack::initMacLayer(MACLayer* macLayer) {
+    IPv6Stack::mac = macLayer;
+    return IPv6Stack::mac->init();
+}
+
+uint8_t IPv6Stack::mac_output(uip_lladdr_t *lladdr) {
+    int numberTrans = 0;
+    rimeaddr_copy((rimeaddr_t*) & ll_destination_address.address, (const rimeaddr_t*) lladdr);
+    uint8_t status = IPv6Stack::mac->send(ll_destination_address, uip_6lp_buf.u8, uip_6lp_len, numberTrans);
+    neighbor_info_packet_sent(status, numberTrans, (const rimeaddr_t*) lladdr);
+    return status;
+}
+
+void IPv6Stack::initIpStack() {
+    uint8_t addr_pos;
+#if ARDUINO_DEBUG
+    arduino_debug_init();
+#endif
+
+    ctimer_init();
+
+    // copy MAC-address
+    for (addr_pos = 0; addr_pos < UIP_LLADDR_LEN; addr_pos++)
+        uip_lladdr.addr[addr_pos] = IPv6Stack::mac->getMacAddress().getAddressValue(addr_pos);
+
+    sicslowpan_init(IPv6Stack::mac_output);
+
+    tcpip_init();
+    clock_init();
+}
+
+void
+IPv6Stack::receiver(struct simple_udp_connection *c,
+        const uip_ipaddr_t *sender_addr,
+        uint16_t sender_port,
+        const uip_ipaddr_t *receiver_addr,
+        uint16_t receiver_port,
+        const uint8_t *data,
+        uint16_t datalen) {
+    IPv6Stack::udp_data = data;
+    IPv6Stack::udp_data_length = datalen;
+    IPv6Stack::udp_data_length_left = datalen;
+    IPv6Stack::sender_addr = sender_addr;
+    IPv6Stack::sender_port = sender_port;
+}
+
+void IPv6Stack::initUdp(uint16_t local_port) {
+    simple_udp_register(&broadcast_connection, local_port, NULL, 0, IPv6Stack::receiver);
+}
+
+void IPv6Stack::udpSend(const IPv6Address &to, uint16_t remote_port, const void *data, uint16_t datalen) {
+    broadcast_connection.remote_port = remote_port;
+    broadcast_connection.udp_conn->rport = UIP_HTONS(remote_port);
+    simple_udp_sendto(&broadcast_connection, data, datalen, &to.address);
+}
+
+bool IPv6Stack::receivePacket() {
+    if (IPv6Stack::mac->receive(ll_source_address, ll_destination_address, uip_6lp_buf.u8, uip_6lp_len)) {
+        rimeaddr_copy((rimeaddr_t*) & ll_dst_addr, (const rimeaddr_t*) &ll_destination_address.address);
+        rimeaddr_copy((rimeaddr_t*) & ll_src_addr, (const rimeaddr_t*) &ll_source_address.address);
+        uip_len = 0;
+        //now decompress
+        input(&ll_src_addr, &ll_dst_addr);
+        if (uip_len != 0) {//if we decompressed sth, update the neighbor info and process the decompressed IPv6 packet
+            neighbor_info_packet_received((const rimeaddr_t*) &ll_src_addr);
+            processIpStack();
+            return true;
+        }
+    }
+    return false;
+}
+
+void IPv6Stack::processIpStack() {
+    return tcpip_input();
+}
+
+void IPv6Stack::pollTimers() {
+    IPv6Stack::udp_data_length = 0;
+    IPv6Stack::udp_data_length_left = 0;
+    etimer_poll();
+}
+
+void IPv6Stack::addAddress(IPv6Address &address) {
+    uip_ds6_addr_add(&address.address, 0, ADDR_AUTOCONF);
+}
+
+#if UIP_CONF_ROUTER
+
+void IPv6Stack::setPrefix(IPv6Address &prefix, uint8_t prefix_length) {
+    uip_ipaddr_t ipaddr;
+    memcpy(&ipaddr, &prefix.address, 16);
+    uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+#if !UIP_CONF_IPV6_RPL
+    uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+    uip_ds6_prefix_add(&prefix.address, prefix_length, 1, UIP_ND6_RA_FLAG_AUTONOMOUS, UIP_ND6_INFINITE_LIFETIME, 0);
+#else /* UIP_CONF_IPV6_RPL */
+    uip_ds6_addr_add(&ipaddr, 0, ADDR_MANUAL);
+    uip_ds6_set_addr_iid(&prefix.address, &uip_lladdr);
+    rpl_dag_t *dag;
+    rpl_set_root((uip_ip6addr_t *) & prefix.address);
+    dag = rpl_get_dag(RPL_ANY_INSTANCE);
+    rpl_set_prefix(dag, &prefix.address, prefix_length);
+#endif /* UIP_CONF_IPV6_RPL */   
+}
+#endif
+
+bool IPv6Stack::getGlobalPreferredAddress(IPv6Address& address) {
+    uip_ds6_addr_t * result = uip_ds6_get_global(ADDR_PREFERRED);
+    if (result != NULL) {
+        address.address = result->ipaddr;
+        return true;
+    }
+    return false;
+}
+
+int IPv6Stack::udpDataAvailable() {
+    return IPv6Stack::udp_data_length_left;
+}
+
+char IPv6Stack::readUdpData() {
+    return IPv6Stack::udp_data[IPv6Stack::udp_data_length - (udp_data_length_left--)];
+}
+
+uint16_t IPv6Stack::getUdpDataLength() {
+    return IPv6Stack::udp_data_length;
+}
+
+void IPv6Stack::getUdpData(char* buffer) {
+    memcpy(buffer, IPv6Stack::udp_data, IPv6Stack::udp_data_length);
+}
+
+int IPv6Stack::getUdpSenderPort() {
+    return IPv6Stack::sender_port;
+}
+
+void IPv6Stack::getUdpSenderIpAddress(IPv6Address &address) {
+    address.address = *IPv6Stack::sender_addr;
+}
+
+void IPv6Stack::ping(IPv6Address &dest, uint8_t datalength) {
+    uip_icmp6_echo_request_output(&dest.address, datalength);
+}
+
+/* sendCoAPpacket function arguments :
+  * IPAddress& address : IP adress of the CoAP server
+  * String uri         : URI you want to access ('/' are not needed)
+  * String query       : URI query optionnal, example : ?query=1
+  * String query       : URI query optionnal, example : &query=2
+  * String payload     : the actual payload
+*/
+
+/*
+ * uri → input name
+ * query → apikey (you have to specify your Read & Write API key
+ * query → node number (optionnal)
+ * payload → sensor reading you want to send
+ */
+
+// send an CoAP request to the time server at the given address
+
+void IPv6Stack::sendCoAPpacket(
+        IPv6Address &address,
+        String &uri,
+        String &query,
+        String &secondQuery,
+        String &payload
+    ) {
+    // Based on draft-ietf-core-coap-13 :  http://tools.ietf.org/html/draft-ietf-core-coap-13
+    //memset(packetBuffer, 0, COAP_PACKET_SIZE * sizeof(byte));
+
+    //packetBuffer = udp_msg_buf; // hack for not changin nae
+
+
+    // Where we are in the packet
+    byte index = 0;
+
+    // Message Format → http://tools.ietf.org/html/draft-ietf-core-coap-13#section-3
+
+    // Initialize values needed to form CoAP header
+    packetBuffer[index++] = 0b01010000; // Version : 1 (01), Type  : Non-Confirmable (01), Option Count : 0 (0000)
+    packetBuffer[index++] = 0b00000011; // Code : PUT (00000011)
+
+
+    // Increment messageID
+    messageID++;
+
+    packetBuffer[index++] = highByte(messageID); // Message ID high byte
+    packetBuffer[index++] = lowByte(messageID); // Message ID low byte
+
+    // options
+    packetBuffer[index++] = 0xB0 | uri.length(); // Uri-Path : 11� http://tools.ietf.org/html/draft-ietf-core-coap-13#section-5.10
+
+    byte i;
+    for (i = 0; i < uri.length(); i++) {
+        packetBuffer[i + index] = uri.charAt(i);
+    }
+    index += i;
+
+    if (query.length() <= 12)
+        packetBuffer[index++] = 0x40 | query.length(); // Uri-Query : 15  ( (DELTA=11) + 4)
+    else {
+        // lenght is greater than 12
+        packetBuffer[index++] = 0x4D; // Uri-Query : 15, Lenght : 13( An 8-bit unsigned integer precedes
+        packetBuffer[index++] = query.length() - 13; // the Option Value and indicates the Option Length minus 13.
+    }
+
+    // uri-query
+    for (i = 0; i < query.length(); i++) {
+        packetBuffer[i + index] = query.charAt(i);
+    }
+    index += i;
+
+    if (secondQuery.length() != 0) {
+
+        if (secondQuery.length() <= 12)
+            packetBuffer[index++] = 0x00 | secondQuery.length(); // Uri-Query : 15
+        else {
+            // lenght is greater than 12
+            packetBuffer[index++] = 0x0D; // Uri-Query : 15, Lenght : 13 A 8-bit unsigned integer precedes
+            packetBuffer[index++] = secondQuery.length() - 13; // the Option Value and indicates the Option Length minus 13.
+        }
+
+        // uri-query
+        for (i = 0; i < secondQuery.length(); i++) {
+            packetBuffer[i + index] = secondQuery.charAt(i);
+        }
+
+        index += i;
+    }
+
+    //****************************************
+    // PAYLOAD
+
+    packetBuffer[index++] = 0xFF; // Payload Marker (0xFF)
+
+    //payload
+    for (i = 0; i < payload.length(); i++) {
+        packetBuffer[i + index] = payload.charAt(i);
+    }
+    index += i;
+
+    // all CoAP fields have been given values, now
+    // you can send a packet requesting a PUT request
+
+    //  IPv6Stack::udp_send(&address, source_port, coap_port, packetBuffer, index);
+    IPv6Stack::udpSend(address, 5683, packetBuffer, index);
+//    IPv6Stack::udpSend(const IPv6Address &to, uint16_t remote_port, const void *data, uint16_t datalen);
+}
